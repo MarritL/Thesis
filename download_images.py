df798bd8f0197660fb4e57f5287184dbf9deadfa
MarritL <marritleenstra@hotmail.com>
2020-01-22 17:20:58 +0100

    running this script, new city points

--- download_images.py
+++ download_images.py
@@ -22,38 +22,46 @@ import time
 size = 600 #image size in pixels
 im = 0 # image for testing
 n_im = 2 # number of images per city
+ntry = 5 # number of times to try to get a sentinel band
 bands = ['B1','B2','B3','B4','B5','B6','B7','B8','B8A','B9','B10','B11','B12']
 data_dir = '/home/cordolo/Documents/Studie Marrit/2019-2020/Thesis/Data'
-data_out = '/media/cordolo/5EBB-8C9A/Studie Marrit/2019-2020/Thesis/data/'
-csv_file = '/media/cordolo/5EBB-8C9A/Studie Marrit/2019-2020/Thesis/S21C_dataset.csv'
+source_dir_cities = '/media/cordolo/elements/source_data/cities'
+results_dir_cities = '/media/cordolo/elements/results/cities'
+intermediate_dir_training = '/media/cordolo/elements/Intermediate/training_S2'
+data_out = '/home/cordolo/Documents/Studie Marrit/2019-2020/Thesis/dataout/'
+#csv_file = '/home/cordolo/Documents/Studie Marrit/2019-2020/Thesis/S21C_dataset.csv'
+#data_out = '/media/cordolo/5EBB-8C9A/Studie Marrit/2019-2020/Thesis/data/'
+#csv_file = '/media/cordolo/5EBB-8C9A/Studie Marrit/2019-2020/Thesis/S21C_dataset.csv'
 
 #%% 
-"Try other dataset"
+"World-cities dataset from geonames"
 import datapackage
 import geocoder
+import random
 
-# variables specific for this part
+# variables 
 minPop = 200000 #min population per city
 data_url = 'https://datahub.io/core/world-cities/datapackage.json'
 
-# to load Data Package into storage
+# data package
 package = datapackage.Package(data_url)
 
-# to load only tabular data
+# load only tabular data
 resources = package.resources
 for resource in resources:
     if resource.tabular:
         data = pd.read_csv(resource.descriptor['path'])
    
-# import data world cities + coordinates 
+# combine with worldcity data from simple maps
 # worldcities.csv downloaded at 06/01/2020 from https://simplemaps.com/data/world-cities
-cities = pd.read_csv(os.path.join(data_dir,'worldcities.csv'), header=0)
+cities = pd.read_csv(os.path.join(source_dir_cities,'worldcities.csv'), header=0)
 
 # data cleaning
 cities_cleaned = cities.drop_duplicates(subset = ['lat', 'lng'])
 cities_cleaned = cities_cleaned.dropna(subset = ['lat', 'lng', 'population'])
 cities_cleaned = cities_cleaned[cities_cleaned['population'] >= minPop]
 
+# merge
 cities_merge = cities_cleaned.merge(data, left_on = ['city','country'], right_on = ['name','country'])
 cities_merge = cities_merge.drop('name', axis=1)
 
@@ -61,11 +69,10 @@ cities_merge = cities_merge.drop('name', axis=1)
 cities_merge['Lng_alt'] = np.nan
 cities_merge['Lat_alt'] = np.nan
 
-
-for idx, row in cities_merge[1004:].iterrows():
+for idx, row in cities_merge.iterrows():
     #time.sleep(1)
     print('\r {}'.format(idx), end='')
-    place = geocoder.geonames(row.geonameid, method='details', key='marrit_l')
+    place = geocoder.geonames(row.geonameid, method='details', key='<api_key>')
     if place == None:
         continue
     if place.feature_class != 'P':
@@ -74,139 +81,82 @@ for idx, row in cities_merge[1004:].iterrows():
     cities_merge.loc[idx,'Lat_alt'] = float(place.lat)
     cities_merge.loc[idx,'Lng_alt'] = float(place.lng)
 
-# save
-cities_merge.to_csv(os.path.join(data_dir,'cities_merge.csv'), index=False)
-
-
-# delete cities that are not found in the OSM database
-cities_cleaned = cities_cleaned.dropna(subset = ['Lng_alt', 'Lat_alt'])
-
-# save
-cities_cleaned.to_csv(os.path.join(data_dir,'cities_cleaned.csv'), index=False)
-
-
-
-
-
-
-#%% 
-"Preprocessing simplemaps world cities dataset"
-
-# imports
-from data_download_functions import placenameToCoordinates
-
-# variables specific for this part
-minPop = 200000 #min population per city
-
-# import data world cities + coordinates 
-# worldcities.csv downloaded at 06/01/2020 from https://simplemaps.com/data/world-cities
-cities = pd.read_csv(os.path.join(data_dir,'worldcities.csv'), header=0)
-
-# data cleaning
-cities_cleaned = cities.drop_duplicates(subset = ['lat', 'lng'])
-cities_cleaned = cities_cleaned.dropna(subset = ['lat', 'lng', 'population'])
-cities_cleaned = cities_cleaned[cities_cleaned['population'] >= minPop]
-
-cities_merge = cities_cleaned.merge(data, left_on = ['city','country'], right_on = ['name','country'])
-cities_merge = cities_merge.drop('name', axis=1)
-#cities_join = cities_cleaned.set_index('city').join(data.set_index('name'), rsuffix='_2')
-
-#%% 
-"Preprocessing world cities dataset"
-
-# imports
-from data_download_functions import placenameToCoordinates
-
-# variables specific for this part
-minPop = 200000 #min population per city
-
-# import data world cities + coordinates 
-# worldcities.csv downloaded at 06/01/2020 from https://simplemaps.com/data/world-cities
-# worldcitiespop.csv downloaded at 13/01/2020 from https://www.kaggle.com/max-mind/world-cities-database
-cities = pd.read_csv(os.path.join(data_dir,'maxmind_world-cities-database/worldcitiespop.csv'), header=0)
-cities = pd.read_csv(os.path.join(data_dir,'worldcities.csv'), header=0)
-
-# data cleaning
-cities_cleaned = cities.drop_duplicates(subset = ['Longitude', 'Latitude'])
-cities_cleaned = cities_cleaned.dropna(subset = ['Longitude', 'Latitude', 'Population'])
-cities_cleaned = cities_cleaned[cities_cleaned['Population'] >= minPop]
-# 1 city contained a typo in the name
-cities_cleaned.loc[cities_cleaned['City'] == 'ulubaria','City'] = 'uluberia'
-cities_cleaned.loc[cities_cleaned['Accentcity'] == 'ulubaria','AccentCity'] = 'Uluberia'
-
-# add alternative lat, lng coordinates
-cities_cleaned['Lng_alt'] = np.nan
-cities_cleaned['Lat_alt'] = np.nan
-for idx, row in cities_cleaned.iterrows():
-    time.sleep(1)
+# check for na's
+cities_na = cities_merge[cities_merge['Lat_alt'].isnull()]
+# check if na's can be filled with other method
+for idx, row in cities_na.iterrows():
+    #time.sleep(1)
     print('\r {}'.format(idx), end='')
-    place = placenameToCoordinates(row.City)
+    place = geocoder.geonames(row.city_ascii+' ('+row.subcountry+')', key='<api_key>')
     if place == None:
         continue
-    cities_cleaned.loc[idx,'Lat_alt'] = place.latitude
-    cities_cleaned.loc[idx,'Lng_alt'] = place.longitude
+    if place.feature_class != 'P':
+        continue
+    
+    cities_merge.loc[idx,'Lat_alt'] = float(place.lat)
+    cities_merge.loc[idx,'Lng_alt'] = float(place.lng)
 
-# delete cities that are not found in the OSM database
-cities_cleaned = cities_cleaned.dropna(subset = ['Lng_alt', 'Lat_alt'])
+#if there are still na's drop these
+cities_merge = cities_merge.dropna(subset = ['Lng_alt', 'Lat_alt'])
 
 # save
-cities_cleaned.to_csv(os.path.join(data_dir,'cities_cleaned.csv'), index=False)
-
+cities_merge = cities_merge.sample(frac=1).reset_index(drop=True)
+cities_merge.to_csv(os.path.join(results_dir_cities,'r_cities.csv'), index=False)
 
 #%%
-from data_download_functions import filterOnCities, eeToNumpy
+from data_download_functions import filterOnCities, eeToNumpy, \
+create_featurecollection
 import csv
+
+data_dir = 'data_S21C'
+csv_file = 'S21C_dataset.csv'
+cities_csv = 'r_cities.csv'
+
+if not os.path.isdir(intermediate_dir_training):
+    os.makedirs(intermediate_dir_training) 
+if not os.path.isdir(os.path.join(intermediate_dir_training, data_dir)):
+    os.makedirs(os.path.join(intermediate_dir_training, data_dir)) 
 
 # init gee session
 #ee.Authenticate()
 ee.Initialize()
 
-#cities = pd.read_csv(os.path.join(data_dir, 'cities_cleaned.csv'))
-#cities = cities[cities['Population'] >= minPop]
-#cities_full = cities
-#cities = cities_merge
-#cities = cities.iloc[:10]
+cities = pd.read_csv(os.path.join(results_dir_cities, cities_csv))
+cities = cities.iloc[332:1000]
 
 # create features
-features = []
-for i in range(len(cities)):
-    #p = ee.Geometry.Point((cities.iloc[i].Longitude, cities.iloc[i].Latitude))
-    p = ee.Geometry.Point((cities.iloc[i].Lng_alt, cities.iloc[i].Lat_alt))
-    f = ee.Feature(p) \
-          .set({'city': cities.iloc[i].city, 'population':cities.iloc[i].population})
-    features.append(f)
-
-# create feature collection
-cities_fc = ee.FeatureCollection(features)
-
+cities_fc = create_featurecollection(cities, ('Lng_alt', 'Lat_alt'), 
+                            properties = ['city_idx','city', 'population'], 
+                            columns = ['city', 'population'])
 
 # get image collection
 S21C = ee.ImageCollection('COPERNICUS/S2').filterBounds(cities_fc) \
     .filterMetadata('CLOUDY_PIXEL_PERCENTAGE','less_than',1) \
     .select(bands)
     
-
 # filter image collection to get 2 random images per city
-cityCollection = cities_fc.map(filterOnCities(S21C, size, n=n_im)) #featurecollection of imagecollection per city
-cityList = cityCollection.toList(cityCollection.size().getInfo()) #List of imagecollection per city
+city_collection = cities_fc.map(filterOnCities(S21C, size, n=n_im)) #featurecollection of imagecollection per city
+city_list = city_collection.toList(city_collection.size().getInfo()) #List of imagecollection per city
+print('cityCollection size: ', city_collection.size().getInfo())
 
-print('cityCollection size: ', cityCollection.size().getInfo())
-
-#imageList = []
-#bands = ["B1", "B2", "B3", "B4"]
-for im in range(len(cityList.getInfo())): 
+n_cities = len(city_list.getInfo())
+tot_time = 0
+for im in range(n_cities): 
     
     start_city = time.time()
-    imagePair = ee.ImageCollection(cityList.get(im))
-    images = imagePair.toList(imagePair.size().getInfo())
+    imagePair = ee.ImageCollection(city_list.get(im))
+    pairsize = imagePair.size().getInfo()
+    if pairsize != n_im:
+        continue
+    images = imagePair.toList(pairsize)
     area = imagePair.geometry()    
     
     for i in range(n_im):
-        start = time.time()
+        start_im = time.time()
         imx = ee.Image(images.get(i))
         
         # get some metadata
-        im_id = str(im)+'_'+chr(ord('a')+i)
+        im_id = imx.get('city_idx').getInfo()+'_'+chr(ord('a')+i)
         system_idx = imx.get('system:index').getInfo()
         city = imx.get('city').getInfo()
         datestr = system_idx.split('_')[0]
@@ -218,26 +168,55 @@ for im in range(len(cityList.getInfo())):
         
         # covert to numpy array
         try: 
-            np_image = eeToNumpy(B, area, bands)
+            np_image = eeToNumpy(B, area, bands, ntry=5)
             # save
-            np.save(data_out+im_id+'.npy', np_image)
+            np.save(os.path.join(intermediate_dir_training,data_dir, im_id+'.npy'), np_image)
         except:
             im_id = 'ERROR'
         
         # save some info in csv-file
-        with open(csv_file, 'a') as file:
+        with open(os.path.join(intermediate_dir_training, csv_file), 'a') as file:
             writer = csv.writer(file, delimiter = ",")
             writer.writerow([im_id, system_idx, city, date, time_im])   
         
-        #imageList.append(np_image)
-        print("1 image: ", time.time() - start)
-    print("1 city: ", time.time() - start_city)
 
-i = 1
-testim = np.load(data_out+'9_'+chr(ord('a')+i)+'.npy')
+        end_im= time.time() - start_im
+        tot_time = tot_time + end_im
+        avg_time = tot_time / ((im*2)+i+1)
+
+        print("\r image {}/{} for city {}/{}. Avg time image (sec): {}".format(i+1,n_im,im+1,n_cities, avg_time), end='')
+
+
+
+
+
+#%%
+i = 55
+j = 1
+testim = np.load(os.path.join(intermediate_dir_training,data_dir,str(i)+'_'+chr(ord('a')+j)+'.npy'))
+np_image = testim
+
+#testim = testim/np.max(testim)
 for i in range(testim.shape[2]):
     testim[:,:,i] = testim[:,:,i]/np.max(testim[:,:,i])
 plt.imshow(testim[:,:,[3,2,1]])
+    
+plt.imshow(np_image[:,:,0])
+plt.imshow(np_image[:,:,1])
+plt.imshow(np_image[:,:,2])
+plt.imshow(np_image[:,:,3])
+plt.imshow(np_image[:,:,4])
+plt.imshow(np_image[:,:,5])
+plt.imshow(np_image[:,:,6])
+plt.imshow(np_image[:,:,7])
+plt.imshow(np_image[:,:,8])
+plt.imshow(np_image[:,:,9])
+plt.imshow(np_image[:,:,10])
+plt.imshow(np_image[:,:,11])
+plt.imshow(np_image[:,:,12])
+
+
+
 #%%
 # =============================================================================
 # def new_test(feature):
@@ -487,4 +466,50 @@ example = cityCollection.map(imagesPerCity)
 print(example.getInfo())
 
 
+tot_time = 0
 
+for im in range(n_cities): 
+
+    imagePair = ee.ImageCollection(city_list.get(im))
+    pairsize = imagePair.size().getInfo()
+    if pairsize != n_im:
+        continue
+    images = imagePair.toList(pairsize)
+    area = imagePair.geometry()    
+    
+    for i in range(n_im):
+        start_im = time.time()
+        imx = ee.Image(images.get(i))
+        
+        # get some metadata
+        im_id = imx.get('city_idx').getInfo()+'_'+chr(ord('a')+i)
+        city = imx.get('city').getInfo()
+        # add the system index (sentinel-2 identifier)
+        system_idx = imx.get('system:index').getInfo()
+        datestr = system_idx.split('_')[0]
+        date = datestr.split('T')[0]
+        time_im = datestr.split('T')[1]
+        
+        # simpify image metadata
+        B = ee.ImageCollection([imx])
+        B = ee.Image(B.median())
+        
+        # covert to numpy array
+        try: 
+            np_image = eeToNumpy(B, area, bands, ntry)
+            # save
+            np.save(os.path.join(intermediate_dir_training, data_dir, im_id+'.npy'), np_image)
+        except Exception as e:
+            print(e)
+            im_id = 'ERROR'
+        
+        # save some info in csv-file
+        with open(os.path.join(intermediate_dir_training, csv_file), 'a') as file:
+            writer = csv.writer(file, delimiter = ",")
+            writer.writerow([im_id, system_idx, city, date, time_im])   
+        
+        end_im= start_im - time.time()
+        tot_time = tot_time + end_im
+        avg_time = tot_time / ((im*2)+i+1)
+
+        print("\r image {}/{} for city {}/{}. Avg time image (sec): {}".format(i+1,n_im,im+1,n_cities, avg_time))
