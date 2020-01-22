b068cd977d6d386dafdbf30e29a6453af7e866e0
MarritL <marritleenstra@hotmail.com>
2020-01-22 17:19:30 +0100

    added function to filter out erroneous images

--- data_download_functions.py
+++ data_download_functions.py
@@ -6,9 +6,9 @@ Created on Tue Jan 14 09:32:14 2020
 @author: M. Leenstra
 """
 import ee
+import os
+import csv
 import numpy as np
-
-
 
 
 def placenameToCoordinates(placename):
@@ -44,7 +44,7 @@ def placenameToCoordinates(placename):
     return getLocation(placename)
 
 
-def clipImage(geom, label, size, resolution=10):
+def clipImage(geom, label, index, size, resolution=10):
     """
     Clips an Image to specified size around a point
 
@@ -54,6 +54,8 @@ def clipImage(geom, label, size, resolution=10):
         Central point of the output image
     label : string
         Name of central point (assumed to be a city)
+    index : int
+        identifier of city
     size : int
         size of output image in pixels
     resolution : int
@@ -68,6 +70,7 @@ def clipImage(geom, label, size, resolution=10):
     def wrap(image):
         nonlocal geom
         nonlocal label
+        nonlocal index
         nonlocal size
         
         #cast to ee objects
@@ -79,6 +82,7 @@ def clipImage(geom, label, size, resolution=10):
         
         #add label and clip to aoi
         image = image.set({'city': label}) \
+                     .set({'city_idx': index})\
                      .clip(aoi) #\
         #             .select('B.+')
     
@@ -119,6 +123,7 @@ def filterOnCities(image_col, size, n=2):
         # get city info
         geometry = city.geometry();
         label = city.getString('city')
+        idx = city.get('city_idx')
         
         # filter imges
         images = image_col.filterBounds(geometry) \
@@ -127,11 +132,16 @@ def filterOnCities(image_col, size, n=2):
                          .limit(n) \
                          .set({'city': label}) \
                          .set({'geometry': geometry}) \
-                         .map(clipImage(geometry, label, size))                    
+                         .set({'city_idx': idx}) \
+                         .map(clipImage(geometry, label, idx, size))  
+         
+        #checksize = images.size().getInfo()    
+        #assert checksize == n
+        #assert images.size().getInfo() >2, "collection is 2"
         return ee.ImageCollection(images) 
     return wrap
 
-def eeToNumpy(img, area, bands=["B4","B3","B2"], resolution=10):
+def eeToNumpy(img, area, bands=["B4","B3","B2"], resolution=10, ntry=5):
     """
     Converts ee.Image to numpy array for specified area and bands. Bands are 
     
@@ -166,28 +176,25 @@ def eeToNumpy(img, area, bands=["B4","B3","B2"], resolution=10):
     lngs = np.array((ee.Array(img.get("longitude")).getInfo()))
     nrow = len(np.unique(lats))
     ncol = len(np.unique(lngs))
-    print(nrow*ncol)
+    #print(nrow*ncol)
 
     # convert image band-by-band to numpy array
     for i, band in enumerate(bands):
-        print(band)
-        #import ipdb
-        #ipdb.set_trace()
+       # print(band)
         # call .get(band) until the right number of pixels is returned
         # (error in gee: sometimes the image is returned only partly)
-        np_b = np.array(range(5), dtype=np.int32)
+        #np_b = np.array(range(5), dtype=np.int32)
+        np_b = np.array(range(5), dtype=np.float)
         n_try = 0
         while np_b.shape[0] != nrow*ncol:
-            np_b = np.array((ee.Array(img.get(band)).getInfo()), dtype=np.int32)
-            print(np_b.shape[0])
+            #np_b = np.array((ee.Array(img.get(band)).getInfo()), dtype=np.int32)
+            np_b = np.array((ee.Array(img.get(band)).getInfo()))
+            #print(np_b.shape[0])
             n_try += 1
-            if n_try > 5:
+            if n_try > ntry:
                 raise Exception('band {} does not have the correct shape'.format(band))
             
         np_b = np_b.reshape(nrow, ncol)
-        
-        # normalize
-        #np_b = np_b / np.max(np_b)
         
         if i == 0:
             np_img = np.zeros([np_b.shape[0], np_b.shape[1], len(bands)])
@@ -196,5 +203,128 @@ def eeToNumpy(img, area, bands=["B4","B3","B2"], resolution=10):
     return np_img
 
 
+def create_featurecollection(df, coords, properties, columns):
+    """
+    create a ee.FeatureCollection of point features out of a dataframe. Note
+    google earth engine should be initialized with account.
+
+    Parameters
+    ----------
+    df : pd.dataframe
+        Dataframe with information about the point features. Should contain at 
+        least the column names specified in coords and columns.
+    coords : tuple
+        tuple or list with the column names for the longitude and latitude in the
+        dataframe.
+    properties : list
+        list of properties (names) for the features. The first feature should be
+        always an index.
+    columns : list
+        list with the column names for the properties specified in 'properties'
+        the first column represents the second property (first property is
+        always the index)
+
+    Returns
+    -------
+    ee.FeatureCollection
+        Feature collection with point features and properties as specified.
+
+    """
+    
+    features = []
+    for idx, row in df.iterrows():
+        # create geometry
+        p = ee.Geometry.Point((df.loc[idx,coords[0]], df.loc[idx,coords[1]]))
+        # create feature with metadata
+        f = ee.Feature(p) \
+              .set({properties[0]: str(idx), properties[1]:df.loc[idx,columns[0]], properties[2]:df.loc[idx,columns[1]]})
+
+        features.append(f)
+
+    # create feature collection
+    return ee.FeatureCollection(features)
+
+def filter_errors_folder(image_folder, threshold=0.5):
+    """
+    This function filters errenous images out of a folder containing images. 
+    It is based on the notion that erroneous images show slightly of horizontal 
+    bands over the image. Hough line detection is used to detect these lines. 
+    The erroneous images are separated from the correct images by thresholding 
+    the standard deviation over the line anlges.     
+
+    Parameters
+    ----------
+    image_folder : string
+        path to folder where images are located
+    threshold : float
+        threshold on standard deviation of angles, under this threshold the 
+        images are considered erroneous. The default is 0.5.
+
+    Returns
+    -------
+    errors : list
+        list with the erroneous image filenames (strings)
+    """
+
+    # list all images in the folder
+    entries = os.listdir(image_folder)
+    
+    errors = filter_errors(image_folder, entries, threshold)
+    
+    return errors
+
+
+def filter_errors(image_folder, image_list, threshold=0.5):
+    """
+    This function filters errenous images out of an the image_list. It is based
+    on the notion that erroneous images show slightly of horizontal bands over
+    the image. Hough line detection is used to detect these lines. The erroneous
+    images are separated from the correct images by thresholding the standard
+    deviation over the line anlges. 
+
+    Parameters
+    ----------
+    image_folder : string
+        path to folder where images are located
+    image_list : list
+        list of image filenames (strings)
+    threshold : float
+        threshold on standard deviation of angles, under this threshold the 
+        images are considered erroneous. The default is 0.5.
+
+    Returns
+    -------
+    errors : list
+        list with the erroneous image filenames (strings)
+        
+    Tested
+    ------
+    bad_images = ['40_a.npy', '163_a.npy', '163_b.npy', '269_a.npy', '313_b.npy', '122_b.npy', '277_a.npy', '328_b.npy', '311_a.npy', '184_b.npy', '398_b.npy', '158_a.npy', '356_b.npy']
+    good_images = ['240_a.npy', '451_b.npy', '558_b.npy','617_a.npy', '61_b.npy','267_b.npy','490_b.npy','243_b.npy', '685_b.npy','696_a.npy']
+    good_images = ['318_a.npy', '527_b.npy', '100_a.npy', '100_b.npy', '101_a.npy', '101_b.npy', '102_a.npy', '102_b.npy', '103_a.npy', '103_b.npy', '104_a.npy', '104_b.npy', '105_a.npy','105_b.npy', '107_a.npy']
+
+    """    
+    
+    from skimage.feature import canny
+    from skimage.transform import hough_line, hough_line_peaks
+    
+
+    errors = []
+    for image in image_list:
+        im = np.load((os.path.join(image_folder,image)))[:,:,0]
+        
+        # edge filter using canny algorithm
+        edges = canny(im, sigma=6)
+        # detect lines using hough transform
+        test_angles = np.linspace(np.pi/4, 7*np.pi/4 / 2, 360/4)
+        h, theta, d = hough_line(edges, theta=test_angles)
+        accum, angle, dist = hough_line_peaks(h, theta, d)
+
+        std = np.std(angle)
+        if std < threshold:
+            errors.extend([image])
+        
+    return errors
+        
 
 
