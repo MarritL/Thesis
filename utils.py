9dfd2f8217830b845ff4886d9164541135665ccb
MarritL <marritleenstra@hotmail.com>
2020-02-05 16:32:03 +0100

    added average meter

--- utils.py
+++ utils.py
@@ -14,3 +14,42 @@ def list_directories(directory):
             dirlist.append(f)
     return dirlist
 
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+    source: https://github.com/CSAILVision/semantic-segmentation-pytorch/blob/master/utils.py
+    """
+    
+    def __init__(self):
+        self.initialized = False
+        self.val = None
+        self.avg = None
+        self.sum = None
+        self.count = None
+
+    def initialize(self, val, weight):
+        self.val = val
+        self.avg = val
+        self.sum = val * weight
+        self.count = weight
+        self.initialized = True
+
+    def update(self, val, weight=1):
+        if not self.initialized:
+            self.initialize(val, weight)
+        else:
+            self.add(val, weight)
+
+    def add(self, val, weight):
+        self.val = val
+        self.sum += val * weight
+        self.count += weight
+        self.avg = self.sum / self.count
+
+    def value(self):
+        return self.val
+
+    def average(self):
+        return self.avg
+
+
