d03ec6dac752f9395cf7d741dc79d7f54d99b353
MarritL <marritleenstra@hotmail.com>
2020-02-25 11:30:42 +0100

    debug to gpu

--- train.py
+++ train.py
@@ -60,12 +60,12 @@ def train(directories, dataset_settings, network_settings, train_settings):
        
     
     ## TODO: load net into GPU (also the data): NOTE: should be done before 
-    # constructing optimizer https://pytorch.org/docs/stable/optim.html
+    # constructing optimizer https://pytorch.org/docs/stable/optim.html    
+    loss_func, acc_func, one_hot = create_loss_function(network_settings['loss'])
     if train_settings['gpu'] != None:
         torch.cuda.set_device(train_settings['gpu'])
         net.cuda()
-    
-    loss_func, acc_func, one_hot = create_loss_function(network_settings['loss'])
+        loss_func.cuda()
     optim = create_optimizer(network_settings['optimizer'], net.parameters(), 
                              network_settings['lr'], 
                              weight_decay=network_settings['weight_decay'])
